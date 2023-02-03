d05981fef0361f60e49e3d3c482f127156497361
Shubham Somnath Mhaskar <124040069+Shubhamm0307@users.noreply.github.com>
2023-02-03 16:12:06 +0530

    Create r1.java

---
+++ r1.java
@@ -0,0 +1,14 @@
+package Day3;
+
+public class r1 {
+
+	public void j1(String o, int q) 
+	{
+		System.out.println("can you");
+
+	}
+	public static void main(String[] args) {
+		r1 g=new r1();
+		g.j1("blue",76);
+	}
+}
